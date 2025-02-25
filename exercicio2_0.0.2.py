814dc1aa66c77b1ff6034dd24003887cdab3b261
PedroColombo2003 <pedrocorrea087@gmail.com>
2025-02-25 20:31:21 -0300

    Add files via upload

---
+++ exercicio2_0.0.2.py
@@ -0,0 +1,27 @@
+"""
+Exercicio 2
+Autor: Pedro Colombo
+Data 25/02/2025
+Versão 0.0.1
+Problema: armazene os numeros 2 e 3, calcule a sua soma e imprima na tela o resultado.
+
+"""
+
+# Alocação de memória
+
+a = 0
+
+b = 0
+
+# Entrada de dados
+
+a = 2
+
+b = 3
+
+# Processamento de dados
+soma = a + b
+
+# Saido de dados
+
+print(soma)
